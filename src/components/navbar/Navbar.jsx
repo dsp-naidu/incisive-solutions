fb32ff3823805fe4ba16b916455b1ad03afb88d7
madhavarayulu <madhavarayulu@gmail.com>
2024-10-04 10:56:58 +0530

    nav dropdown implementation 1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './navbar.css';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 
 function Navbar() {
+  const [showProductsDropdown, setShowProductsDropdown] = useState(false);
+  const [showServicesDropdown, setShowServicesDropdown] = useState(false);
+
+  const handleProductsEnter = () => setShowProductsDropdown(true);
+  const handleProductsLeave = () => setShowProductsDropdown(false);
+  const handleServicesEnter = () => setShowServicesDropdown(true);
+  const handleServicesLeave = () => setShowServicesDropdown(false);
+
   return (
     <nav className="navbar">
       {/* <div className="navbar-logo">
@@ -21,7 +29,8 @@ function Navbar() {
             src="logo-incisive-solutions.png"
             alt="Incisive Solutions Logo"
           /> */}
-        <h1>Incisive Solutions
+        <h1>
+          Incisive Solutions
           {/* <span className="brand-color-1">Incisive </span>
           <span className="brand-color-2">Solutions</span> */}
         </h1>
@@ -38,19 +47,71 @@ function Navbar() {
           Home
         </NavLink>
 
-        <NavLink to="products" className="navbar-menu-item">
-          Products
+        <div
+          className="dropdown"
+          onMouseEnter={handleProductsEnter}
+          onMouseLeave={handleProductsLeave}
+        >
+          <NavLink to="products" className="navbar-menu-item">
+            Products
+          </NavLink>
           {/* <span>
-            <RiArrowDropDownLine className="dropdown-arrow" />
+            Products <RiArrowDropDownLine className="dropdown-arrow" />
           </span> */}
-        </NavLink>
+          {showProductsDropdown && (
+            <div className="dropdown-menu">
+              <Link to="/products/product1" className="dropdown-item">
+                Product 1
+              </Link>
+              <Link to="/products/product2" className="dropdown-item">
+                Product 2
+              </Link>
+              <Link to="/products/product3" className="dropdown-item">
+                Product 3
+              </Link>
+            </div>
+          )}
+        </div>
 
-        <NavLink to="services" className="navbar-menu-item">
-          Services
+        <div
+          className="dropdown"
+          onMouseEnter={handleServicesEnter}
+          onMouseLeave={handleServicesLeave}
+        >
+          <NavLink to="services" className="navbar-menu-item">
+            Services
+          </NavLink>
           {/* <span>
-            <RiArrowDropDownLine className="dropdown-arrow" />
+            Services <RiArrowDropDownLine className="dropdown-arrow" />
           </span> */}
-        </NavLink>
+          {showServicesDropdown && (
+            <div className="dropdown-menu">
+              <Link to="/services/service1" className="dropdown-item">
+                Service 1
+              </Link>
+              <Link to="/services/service2" className="dropdown-item">
+                Service 2
+              </Link>
+              <Link to="/services/service3" className="dropdown-item">
+                Service 3
+              </Link>
+            </div>
+          )}
+        </div>
+
+        {/* <NavLink to="products" className="navbar-menu-item">
+          Products
+          <span>
+            <RiArrowDropDownLine className="dropdown-arrow" />
+          </span>
+        </NavLink> */}
+
+        {/* <NavLink to="services" className="navbar-menu-item">
+          Services
+          <span>
+            <RiArrowDropDownLine className="dropdown-arrow" />
+          </span>
+        </NavLink> */}
         {/* <select>
           <option>BPO Services</option>
           <option>Data Entry Services</option>
