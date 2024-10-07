9a7dfe7292eab6f684722d25c20c21401774f954
madhavarayulu <madhavarayulu@gmail.com>
2024-10-07 23:47:46 +0530

    5th working draft

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -2,7 +2,41 @@ import React from 'react';
 import './team.css';
 
 function Team() {
-  return <div className='team-container'>Team</div>;
+  return (
+
+    <div className="team-container">
+      <div className="team-hero-container">
+        <video autoPlay muted loop playsInline className="background-video">
+          <source src="/assets/team-bg.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="team-hero-heading">
+          <h1>Our Team</h1>
+        </div>
+      </div>
+{/* 
+      <div className="team-content-wrapper">
+        <div className='team-content'>
+          <div>
+          <p>We are outfitted to lighten your daily data concerns.</p>
+          </div>
+
+          <div>
+          <p>We are outfitted to lighten your daily data concerns.</p>
+          </div>
+
+          <div>
+          <p>We are outfitted to lighten your daily data concerns.</p>
+          </div>
+
+          <div>
+          <p>We are outfitted to lighten your daily data concerns.</p>
+          </div>
+        </div>
+      </div> */}
+
+    </div>
+  );
 }
 
 export default Team;
