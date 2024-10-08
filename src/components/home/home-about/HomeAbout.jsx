fc9e4079404530c0984a3e28915f53b7ec6f8df0
madhavarayulu <madhavarayulu@gmail.com>
2024-10-09 00:11:03 +0530

    6th working draft

--- src/components/home/home-about/HomeAbout.jsx
+++ src/components/home/home-about/HomeAbout.jsx
@@ -4,44 +4,53 @@ import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 function HomeAbout() {
   return (
-    <div className="home-about-container">
-      <div className="home-about-heading">
-        <h1>INCISIVE SOLUTIONS</h1>
-        <h2>AN OFFSHORE OUTSOURCING COMPANY</h2>
-      </div>
+    <div className="home-about-wrapper">
+      <div className="home-about-container">
+        <div className="home-about-heading">
+          <h1>INCISIVE SOLUTIONS</h1>
+          <h2>AN OFFSHORE OUTSOURCING COMPANY</h2>
+        </div>
 
-      <div className="home-about-content">
-        <DotLottieReact
-          className="home-about-animate-object"
-          src="company-graph.json"
-          loop
-          autoplay
-        />
+        <div className="home-about-content">
+          <DotLottieReact
+            className="home-about-animate-object"
+            src="company-graph.json"
+            loop
+            autoplay
+          />
 
-        <div className="home-about-section-content">
-          <h3>
-            Looking for a reliable and accurate outsource services provider?
-            Then your search ends here!
-          </h3>
-          <p>
-            Incisive Solutions is well known for providing customized data entry
-            services with high quality standards. We mainly focus on providing
-            accurate and reliable data entry services to our customers with a
-            short span of time. With 100% client retention ratio, we are
-            successful in accomplishing the satisfaction of our customers. We
-            have a fabulous team who provides the best data entry outsourcing
-            services without compromising on the quality. The use of latest
-            technology also ensures that the customers get their desired output
-            without any additional payments. Incisive Solutions is a leading
-            offshore outsourcing company with well specialized data entry
-            specialists entrusting you with 100% accuracy in data entry. Dealing
-            with Incisive Solutions helps you to widen your base knowledge about
-            several service industries and markets, and helps you to take apt
-            decisions. We have handled numerous small and large scale companies
-            and is always a constant support for their planning and development.
-            We believe in the quote: “Always deliver the most expected” and we
-            ensure that what you expects is delivered in the best possible way.
-          </p>
+          <div className="home-about-section-content">
+            <h3>
+              Looking for a reliable and accurate outsource services provider?
+              Then your search ends here!
+            </h3>
+            <p>
+              Incisive Solutions is well known for providing customized data
+              entry services with high quality standards. We mainly focus on
+              providing accurate and reliable data entry services to our
+              customers with a short span of time. With 100% client retention
+              ratio, we are successful in accomplishing the satisfaction of our
+              customers.
+            </p>
+            <p>
+              We have a fabulous team who provides the best data entry
+              outsourcing services without compromising on the quality. The use
+              of latest technology also ensures that the customers get their
+              desired output without any additional payments. Incisive Solutions
+              is a leading offshore outsourcing company with well specialized
+              data entry specialists entrusting you with 100% accuracy in data
+              entry.
+            </p>
+            <p>
+              Dealing with Incisive Solutions helps you to widen your base
+              knowledge about several service industries and markets, and helps
+              you to take apt decisions. We have handled numerous small and
+              large scale companies and is always a constant support for their
+              planning and development. We believe in the quote: “Always deliver
+              the most expected” and we ensure that what you expects is
+              delivered in the best possible way.
+            </p>
+          </div>
         </div>
       </div>
     </div>
